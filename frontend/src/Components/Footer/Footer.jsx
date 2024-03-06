23122dcda4c4b30e6f48d4cc39b9a7f419336a6b
devbhardwaj012 <141846972+devbhardwaj012@users.noreply.github.com>
2024-03-06 14:29:27 +0530

    i added the navbar and footer

---
+++ frontend/src/Components/Footer/Footer.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <footer class="footer">
+  <div class="container">
+    <div class="footer-info">
+      <h3>Contact Us</h3>
+      <p>If you have any questions or need support, feel free to reach out to us:</p>
+      <ul>
+        <li>Email: contactus@example.com</li>
+        <li>Phone: +91 9034567832</li>
+      </ul>
+    </div>
+
+  </div>
+</footer>
+
+  )
+}
+
+export default Footer
--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,31 +1,21 @@
 import React from 'react'
 import './Navbar.css'
-import cart_icon from '../Assets/cart_icon.png'
-import ubus from '../Assets/ubus.png'
+
 
  const Navbar = () => {
 
   return (
-    <div className='navbar'>
-        <div className='nav-logo'>
-            <img src={ubus} alt="" />
-            <p>UB&US</p>
-        </div>
+    <nav class="navbar">
+  <div class="container">
+    <a href="#" class="logo">Evaluation Website</a>
+    <ul class="nav-links">
+      <li><a href="#">Home</a></li>
+      <li><a href="#">Products</a></li>
+    </ul>
+  </div>
+</nav>
 
-        <ul className ="nav-menu">
-            <li>Shop <hr /></li>
-            <li>latop</li>
-            <li>bicycle</li>
-            <li>smartphone</li>
-        </ul>
-
-        <div className="nav-login-cart">
-            <button>Login</button>
-            <img src={cart_icon} alt="" />
-        </div>
-
-    </div>
-  )
+    )
 }
 
 
