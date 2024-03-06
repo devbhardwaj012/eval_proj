415b2c1fbb8cab0b14c2531bb7e0d415c49c46a5
Sarverjii <sarverjii@gmail.com>
2024-03-06 16:46:41 +0530

    Adding A testing Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 import Footer from './Components/Footer/Footer';
+import Testing from './Components/Testing/Testing';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Testing/>
       <Footer/>
  
     </div>
